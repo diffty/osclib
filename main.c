ab2b8a4c0987cdae42be1b1b8baeb5746a3a9185
DiFFtY <diffty@gmail.com>
2023-12-22 01:59:40 +0100

    nouvelle formule avec get_arg_to_osc_message et add_arg_to_osc_message
    read_data ne malloc plus

--- main.c
+++ main.c
@@ -3,8 +3,18 @@
 #include <string.h>
 
 
-// Contrary to `write_data`, pDstValue memory block is allocated by
-// the function `read_data` itself. Beware of leaks.
+int calculate_size_with_padding(int size) {
+    int paddingSize = size % 4;
+    if (paddingSize > 0) {
+        return size + (4 - paddingSize);
+    }
+    else {
+        return size;
+    }
+}
+
+
+// pDstValue memory block is not allocated by the function `read_data` itself
 void read_data(void* pDstValue, char valueType, void** pDataCursor) {
     switch (valueType) {
         case 'i': {
@@ -31,21 +41,21 @@ void read_data(void* pDstValue, char valueType, void** pDataCursor) {
 
             int paddingSize = 3 - (strSize % 4);
 
-            char* pNewStr = (char*) malloc(strSize * sizeof(char));
+            //char* pNewStr = (char*) malloc(strSize * sizeof(char));
 
-            memset(pNewStr, '\0', strSize);
-            memcpy(pNewStr, *pDataCursor, strSize);
+            memset(pDstValue, '\0', strSize);
+            memcpy(pDstValue, *pDataCursor, strSize);
 
-            *((char**) pDstValue) = pNewStr;
+            //*((char**) pDstValue) = pNewStr;
 
-            *pDataCursor = pStrCursor + strSize + paddingSize;
+            *pDataCursor = pDataCursor + strSize + paddingSize;
             break;
         }
     }
 }
 
-// Contrary to `read_data`, the destination message memory block
-// at `pDataIteratorPtr` is NOT allocated before it's written into.
+// The destination message memory block at `pDataIteratorPtr` is NOT allocated
+// before it's written into.
 void write_data(void* pSrcValue, char valueType, void** pDataBlockPtr) {
     switch (valueType) {
         case 'i': {
@@ -79,6 +89,77 @@ void write_data(void* pSrcValue, char valueType, void** pDataBlockPtr) {
     }
 }
 
+typedef struct OscMessage {
+    char* address;
+    void* argsData;
+    char* argsTypes;
+    int* argsDataIndexes;
+    int argsDataSize;
+} OscMessage;
+
+
+int get_arg_value_size(void* pArgData, char argType) {
+    switch (argType) {
+        case 'i': {
+            return sizeof(int32_t);
+        }
+
+        case 'f': {
+            return sizeof(float);
+        }
+
+        case 'b': {
+            return sizeof(char);
+        }
+
+        case 's': {
+            int strSize = 0; while (((char*) pArgData)[strSize++] != '\0');
+            return strSize;
+        }
+    }
+}
+
+void add_arg_to_osc_message(OscMessage* pMessage, void* pArgData, char argType) {
+    void* oldArgsData = pMessage->argsData;
+    void* oldArgsTypes = pMessage->argsTypes;
+
+    int nbArgs = strlen(pMessage->argsTypes);
+    int argValueSize = get_arg_value_size(pArgData, argType);
+
+    pMessage->argsData = (void*) malloc(calculate_size_with_padding(pMessage->argsDataSize + argValueSize));
+    pMessage->argsTypes = (char*) malloc(calculate_size_with_padding(nbArgs+2));
+
+    memset(pMessage->argsData, '\0', calculate_size_with_padding(pMessage->argsDataSize + argValueSize));
+
+    if (oldArgsData != NULL) {
+        memmove(pMessage->argsData, oldArgsData, pMessage->argsDataSize);
+    }
+    
+    memcpy((char*) pMessage->argsData + pMessage->argsDataSize, pArgData, argValueSize);
+    memcpy(pMessage->argsTypes, oldArgsTypes, nbArgs);
+
+    pMessage->argsDataSize += calculate_size_with_padding(argValueSize);
+    pMessage->argsTypes[nbArgs] = argType;
+}
+
+void* get_arg_in_osc_message(OscMessage* pMessage, int argId) {
+    void* pDataCursor = pMessage->argsData;
+    void* pCurrValue = NULL;
+
+    int i;
+    for (i = 0; i <= argId; i++) {
+        int dataSize = get_arg_value_size(&pDataCursor, pMessage->argsTypes[i]);
+        pCurrValue = (void*) malloc(dataSize);
+        read_data(pCurrValue, pMessage->argsTypes[i], &pDataCursor);
+
+        if (i < argId) {
+            free(pCurrValue);
+        }
+    }
+
+    return pCurrValue;
+}
+
 void print_memory_block_hex(void* pMemBlock, int memBlockSize) {
     int i;
     
@@ -97,7 +178,49 @@ int calculate_str_size_with_padding(char* s) {
 
 // DC FF 00 00 45 00 00 00 00 00 00 40 74 65 73 74 61 69 6E 74 00 00 00 00 
 
+
+int main2() {
+    printf("--- MAIN 2 ---\n");
+    OscMessage message;
+
+    char address[] = "/test";
+    message.address = (char*) malloc(strlen(address) + 1);
+    strcpy(message.address, address);
+    message.argsData = NULL;
+    message.argsTypes = (char*) malloc(1);
+    message.argsTypes[0] = '\0';
+    message.argsDataSize = 0;
+
+    int arg1 = 69;
+    add_arg_to_osc_message(&message, &arg1, 'i');
+
+    float arg2 = 2.0;
+    add_arg_to_osc_message(&message, &arg2, 'f');
+
+    char keveune[] = "abdefh";
+    add_arg_to_osc_message(&message, &keveune, 's');
+
+    int arg3 = 1337;
+    add_arg_to_osc_message(&message, &arg3, 'i');
+
+    printf("%s %d\n", message.argsTypes, message.argsDataSize);
+    print_memory_block_hex(message.argsData, message.argsDataSize);
+
+    char* texte = (char*) get_arg_in_osc_message(&message, 2);
+    printf("%s\n", texte);
+
+    float* test2 = (float*) get_arg_in_osc_message(&message, 1);
+    printf("%f\n", *test2);
+
+    free(texte);
+    free(test2);
+
+    print_memory_block_hex(message.argsData, message.argsDataSize);
+}
+
+
 int main() {
+    printf("--- MAIN 1 ---\n");
     int32_t value = 65500;
     int32_t value2 = 69;
     float value3 = 2.;
@@ -115,21 +238,39 @@ int main() {
     write_data(&value3, 'i', &pDataPtr);
     write_data(&value4, 's', &pDataPtr);
 
-    int32_t v1;
-    int32_t v2;
-    float vf;
-    char* vs;
-
     pDataPtr = pData;
 
-    read_data(&v1, 'i', &pDataPtr);
-    read_data(&v2, 'i', &pDataPtr);
-    read_data(&vf, 'f', &pDataPtr);
-    read_data(&vs, 's', &pDataPtr);
+    int32_t* v1 = (int32_t*) malloc(get_arg_value_size(pDataPtr, 'i'));
+    read_data(v1, 'i', &pDataPtr);
 
-    printf("%d %d %f %s\n", v1, v2, vf, vs);
+    int32_t* v2 = (int32_t*) malloc(get_arg_value_size(pDataPtr, 'i'));
+    read_data(v2, 'i', &pDataPtr);
+    
+    float* vf = (float*) malloc(get_arg_value_size(pDataPtr, 'f'));
+    read_data(vf, 'f', &pDataPtr);
+
+    char* vs = (char*) malloc(get_arg_value_size(pDataPtr, 's'));
+    read_data(vs, 's', &pDataPtr);
+
+    printf("%d %d %f %s\n", *v1, *v2, *vf, vs);
+
+    free(pData);
+    free(v1);
+    free(v2);
+    free(vf);
+    free(vs);
 
     print_memory_block_hex(pData, dataSize);
 
+    main2();
+
     return 0;
+}
+
+
+int main3() {
+    int i;
+    for (i = 0; i < 20; i++)
+        printf("%d %d\n", i, calculate_size_with_padding(i));
+    
 }
